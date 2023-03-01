4c01f27f046b6ecad6d11afd17261b0d32e9ce49
sirirat <119990213+yokksp@users.noreply.github.com>
2023-03-01 20:25:17 +0700

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function submit() {
+  display = document.getElementById("display");
+  var output = "";
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var dateOfBirth = document.getElementById("dateOfBirth").value;
+  var language = document.getElementById("language").value;
+  console.log(name, surname, dateOfBirth, language);
+  if (name == "" || surname == "" || dateOfBirth == "" || language == "") {
+    display.style.color = "red";
+
+    if (name == "") {
+      output += "Insert Name <br>";
+    }
+    if (surname == "") {
+      output += "Insert Surname <br>";
+    }
+    if (dateOfBirth == "") {
+      output += "Insert Birth Date <br>";
+    }
+    if (language == "") {
+      output += "Select Language <br>";
+    }
+  } else {
+    display.style.color = "green";
+    output += name + "," + surname + "," + dateOfBirth + "," + language;
+  }
+
+  display.innerHTML = output;
+}
